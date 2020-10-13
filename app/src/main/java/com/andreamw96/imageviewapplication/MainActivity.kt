7da471e3145e74cfda8da3be4c6644a97d95cf00
andremw96 <andre19961@gmail.com>
2020-10-13 10:05:14 +0700

    Initial commit

---
+++ app/src/main/java/com/andreamw96/imageviewapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andreamw96.imageviewapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
